001ea2e50f89517d1c0b571a0986bad30a514e71
Naimesh Rana <naimesh007rana@gmail.com>
2023-11-26 20:09:27 +0530

    Adding database project

---
+++ src/main/java/com/simplilearn/DataBaseProject/App.java
@@ -0,0 +1,13 @@
+package com.simplilearn.DataBaseProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
